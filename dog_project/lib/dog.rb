12c60c29c54df08c639ea01c0d1b14288953f216
Thomas Conger <t.e.conger@gmail.com>
2022-10-10 13:48:56 -0700

    Dog project initial

---
+++ dog_project/lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+    def initialize(name, breed, age, bark, favoroite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+end
