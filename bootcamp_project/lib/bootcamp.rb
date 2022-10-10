711494a8abe0c886c6cf926b962f1491123eb0b8
Thomas Conger <t.e.conger@gmail.com>
2022-10-10 14:55:07 -0700

    completed bootcamp project

---
+++ bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,9 @@
+class Bootcamp
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @students = []
+        @teachers = []
+    end
+end
